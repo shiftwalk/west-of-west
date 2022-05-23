6bcb8a8f85ea8e63ace40bfaccbea0ddb265a3d4
Sam Goddard <hello@samgoddard.co.uk>
2022-05-23 15:08:38 +0100

    scaffolding

--- pages/dev-sandbox.js
+++ pages/dev-sandbox.js
@@ -32,25 +32,25 @@ export default function Dev() {
               <p>Various work in progress components and playground for dev testing. Not representative of final development, accessibility or design.</p>
             </div>
             
-            <div className="my-16 md:my-24 content max-w-3xl">
-              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">.obj Viewer</h2>
+            <div className="my-16 md:my-24 xl:my-36 content max-w-3xl">
+              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">↘ .obj Viewer</h2>
               <p>Example of loading in an .obj file from the CMS.</p>
 
-              <div className="bg-gray-100 p-3 max-w-[550px] mt-4 md:mt-6">
+              <div className="bg-gray bg-opacity-[15%] p-2 max-w-[550px] mt-4 md:mt-6">
                 <OBJModel src="/3d/test.obj" />
               </div>
             </div>
 
-            <div className="my-16 md:my-24">
-              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">Indent Headings</h2>
+            <div className="my-16 md:my-24 xl:my-36">
+              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">↘ Indent Headings</h2>
             
               <p>Example of larger viewport width driven, indented headings</p>
               
               <span className="block indent-[20%] text-[9vw] md:text-[7.25vw] xl:text-[5.85vw] leading-none md:leading-none xl:leading-none max-w-[80vw] mt-4 md:mt-6">We are an architecture &amp; design studio that creates experience driven buildings and interiors.</span>
             </div>
 
-            <div className="my-16 md:my-24 content max-w-3xl">
-              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">Inline Links</h2>
+            <div className="my-16 md:my-24 xl:my-36 content max-w-3xl">
+              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">↘ Inline Links</h2>
               
               <p>Example of inline link hover / focus styling.</p>
 
@@ -68,12 +68,12 @@ export default function Dev() {
               </div>
             </div>
 
-            <div className="my-16 md:my-24 content">
-              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">Teaser Hovers</h2>
+            <div className="my-16 md:my-24 xl:my-36 content">
+              <h2 className="text-xl md:text-2xl xl:text-3xl mb-4 mt-12">↘ Teaser Hovers</h2>
               
               <p>Example of teaser hover / focus styles</p>
-              <div className="flex space-x-5 mt-4 md:mt-6">
-                <a href="#" className="block w-1/3 group">
+              <div className="flex flex-wrap md:flex-nowrap md:space-x-5 mt-4 md:mt-6 max-w-[80vw]">
+                <a href="#" className="block w-full md:w-1/3 group mb-4 md:mb-0">
                   <div className="relative overflow-hidden mb-3">
                     <img src="images/test.jpg" className="w-full" alt="test" />
                     <div className="bg-black bg-opacity-40 flex items-center justify-center absolute inset-0 z-10 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity ease-in-out duration-500">
@@ -84,7 +84,7 @@ export default function Dev() {
                   <span className="block text-lg leading-none text-gray">Architecture</span>
                 </a>
 
-                <a href="#" className="block w-1/3 group">
+                <a href="#" className="block w-full md:w-1/3 group mb-4 md:mb-0">
                   <div className="relative overflow-hidden mb-3">
                     <img src="images/test.jpg" className="w-full" alt="test" />
                     <div className="bg-black bg-opacity-40 absolute inset-0 z-10 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity ease-in-out duration-500 p-3">
@@ -101,7 +101,7 @@ export default function Dev() {
                   </div>
                 </a>
 
-                <a href="#" className="block w-1/3 group">
+                <a href="#" className="block w-full md:w-1/3 group mb-4 md:mb-0">
                   <div className="relative overflow-hidden mb-3">
                     <img src="images/test.jpg" className="w-full" alt="test" />
                     <div className="bg-black bg-opacity-40 flex items-center justify-center absolute inset-0 z-10 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity ease-in-out duration-500">
