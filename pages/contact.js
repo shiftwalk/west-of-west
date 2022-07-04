24f0e265f832e412dcbefb1343b75335b827b900
Sam Goddard <hello@samgoddard.co.uk>
2022-07-04 18:04:27 +0100

    home page hero interaction, slug hero pin, map interaction

--- pages/contact.js
+++ pages/contact.js
@@ -7,10 +7,10 @@ import { NextSeo } from 'next-seo'
 import SanityPageService from '@/services/sanityPageService'
 import { useContext, useEffect, useState, useRef } from 'react'
 import { IntroContext } from 'context/intro'
-import mapboxgl from 'mapbox-gl';
+// import mapboxgl from 'mapbox-gl';
 import geoJson from "../helpers/locations.json";
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+// mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 const query = `{
   "contact": *[_type == "contact"][0]{
@@ -45,62 +45,59 @@ export default function Contact(initialData) {
   const [hovering, setHovering] = useState(false);
   const [introContext, setIntroContext] = useContext(IntroContext);
   const [currentLocation, setCurrentLocation] = useState(0);
-  const mapContainerRef = useRef(null);
+  // const mapContainerRef = useRef(null);
 
   useEffect(() => {
     setIntroContext(true)
 
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/shiftwalk/cl56sqsa300cj14og2a4vsrfi",
-      center: [-122.664238, 45.513533],
-      zoom: 14,
-    });
+    // const map = new mapboxgl.Map({
+    //   container: mapContainerRef.current,
+    //   style: "mapbox://styles/shiftwalk/cl56sqsa300cj14og2a4vsrfi",
+    //   center: [-122.664238, 45.513533],
+    //   zoom: 14,
+    // });
 
-    // Create default markers
-    geoJson.features.map((feature) => {
-      const el = document.createElement('div');
-      const width = 65;
-      const height = 65;
-      el.className = 'marker';
-      el.style.backgroundImage = `url(/images/marker.svg)`;
-      el.style.width = `${width}px`;
-      el.style.height = `${height}px`;
-      el.style.backgroundSize = '100%';
+    // geoJson.features.map((feature) => {
+    //   const el = document.createElement('div');
+    //   const width = 65;
+    //   const height = 65;
+    //   el.className = 'marker';
+    //   el.style.backgroundImage = `url(/images/marker.svg)`;
+    //   el.style.width = `${width}px`;
+    //   el.style.height = `${height}px`;
+    //   el.style.backgroundSize = '100%';
       
-      new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map)
-    });
+    //   new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map)
+    // });
 
-    // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+    // map.addControl(new mapboxgl.NavigationControl(), "top-right");
     
-    const moveToLa = () => {
-      setCurrentLocation(1)
-      map.flyTo({
-        center: [-118.255327, 34.083433],
-        essential: true,
-        curve: 1.4,
-        speed: 1.3,
-        zoom: 14
-      });
-    }
+    // const moveToLa = () => {
+    //   setCurrentLocation(1)
+    //   map.flyTo({
+    //     center: [-118.255327, 34.083433],
+    //     essential: true,
+    //     curve: 1.4,
+    //     speed: 1.3,
+    //     zoom: 14
+    //   });
+    // }
     
-    const moveToPortland = () => {
-      setCurrentLocation(0)
-      map.flyTo({
-        center: [-122.664238, 45.513533],
-        essential: true,
-        curve: 1.4,
-        speed: 1.3,
-        zoom: 14
-      });
-    }
+    // const moveToPortland = () => {
+    //   setCurrentLocation(0)
+    //   map.flyTo({
+    //     center: [-122.664238, 45.513533],
+    //     essential: true,
+    //     curve: 1.4,
+    //     speed: 1.3,
+    //     zoom: 14
+    //   });
+    // }
 
-    document.getElementById("location-1").addEventListener("click", moveToLa);
-    document.getElementById("location-0").addEventListener("click", moveToPortland);
+    // document.getElementById("location-1").addEventListener("click", moveToLa);
+    // document.getElementById("location-0").addEventListener("click", moveToPortland);
 
-    // Clean up on unmount
-    return () => map.remove();
+    // return () => map.remove();
   }, []);
 
   return (
@@ -138,7 +135,7 @@ export default function Contact(initialData) {
 
               <div className="col-span-10 md:col-span-8 pb-2">
                 <div className="w-full h-[66vw] md:h-full bg-white relative overflow-hidden">
-                  <div className="absolute inset-0 w-full h-full object-cover" ref={mapContainerRef} />
+                  {/* <div className="absolute inset-0 w-full h-full object-cover" ref={mapContainerRef} /> */}
                 </div>
               </div>
             </div>
