0ce4793b8874cf2cfb70d548a846387c398eabb3
Sam Goddard <hello@samgoddard.co.uk>
2022-07-19 11:24:58 +0100

    testing cursor dot

--- components/teaser.js
+++ components/teaser.js
@@ -2,7 +2,7 @@ import Image from "@/components/image";
 
 export default function Teaser({ height, image, isActive, position, external, preload }) {
   return(
-    <div className={`mb-3 relative overflow-hidden ${height} cursor-none`}>
+    <div className={`mb-3 relative overflow-hidden ${height} cursor-crosshair`}>
       <Image
         image={image}
         focalPoint={image.hotspot}
@@ -13,7 +13,7 @@ export default function Teaser({ height, image, isActive, position, external, pr
       />
       
       <div className="bg-black bg-opacity-40 flex items-center justify-center absolute inset-0 z-[10001] opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity ease-in-out duration-[250ms]">
-        <svg className={`w-10 absolute pointer-events-none ${isActive ? 'opacity-100' : 'opacity-0'}`} style={{ top: position.y - 20, left: position.x - 20 }} viewBox="0 0 34 36" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="#fff" strokeWidth="2" d="M34 17.903H0M16.822 35.002V.998"/></svg>
+        {/* <svg className={`w-10 absolute pointer-events-none ${isActive ? 'opacity-100' : 'opacity-0'}`} style={{ top: position.y - 20, left: position.x - 20 }} viewBox="0 0 34 36" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke="#fff" strokeWidth="2" d="M34 17.903H0M16.822 35.002V.998"/></svg> */}
       </div>
     </div>
   )
